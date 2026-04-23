0e50fd82932a7bf277db1e1fdab23215d4075bbb
Alexa <kubyashka.0183@gmail.com>
2026-04-23 14:42:51 +0500

    update bot logic

--- main.py
+++ main.py
@@ -45,4 +45,17 @@ async def main():
 if __name__ == '__main__':                       #запуск
     asyncio.run(main())
     
+#для сервера или сайта чтоб бот не засыпал 
+from flask import Flask
+import threading
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is alive"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+threading.Thread(target=run).start()
